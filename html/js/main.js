268606a42a468ec1fbaefa71f4d1fb22c7e71537
이지언 <80095827+2jieon@users.noreply.github.com>
2023-11-13 20:58:31 +0900

    Update main.js

--- html/js/main.js
+++ html/js/main.js
@@ -520,7 +520,7 @@ $(document).ready(function(){
             animTimeouts = [];
             animIntervals = [];
             if (window.matchMedia("(min-width: 1800px)").matches) {
-            visualAnim(295, 418, 255);
+            visualAnim(255, 388, 215);
             } else if (window.matchMedia("(min-width: 1601px)").matches) {
             visualAnim(294, 408, 265);
             } else if (window.matchMedia("(min-width: 1201px)").matches) {
